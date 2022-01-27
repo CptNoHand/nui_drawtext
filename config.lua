564fb5a1568e85e442191670c53e09c70245ed71
loljoshie <91661118+loljoshie@users.noreply.github.com>
2022-01-26 22:54:32 -0700

    Delete config.lua

--- config.lua
+++
@@ -1,5 +0,0 @@
-Config = {}
-
-Config.AdminOnly = false -- Only Admins will be able to use this NUI
-Config.OpenKeyActiveLaser = 'K' -- https://docs.fivem.net/docs/game-references/input-mapper-parameter-ids/keyboard/
-Config.OpenKeyDeleteLaser = 'DELETE' -- https://docs.fivem.net/docs/game-references/input-mapper-parameter-ids/keyboard/
